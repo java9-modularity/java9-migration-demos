74dcfe30620ac53d5277f531f040b34cae6528d1
Paul Bakker <paul.bakker@luminis.eu>
2016-08-31 19:25:15 +0200

    Demo 3 step 2 - moved some libs to -mp and added requires to module-info

--- 3_MonolithicSpring/src/bookapp/module-info.java
+++ 3_MonolithicSpring/src/bookapp/module-info.java
@@ -1,3 +1,11 @@
 module bookapp {
-
+	requires spring.context;
+	requires spring.tx;
+	
+	requires javax.inject;
+	
+	requires hibernate.core;
+	requires hibernate.jpa;
+	
+	requires java.naming;	
 }

4c10233eeca7804bfc07fe7e313674fa0ed7f658
Sander Mak <sander.mak@luminis.eu>
2016-09-16 11:14:31 +0200

    Tidy up output

--- 2_Modules/src/main/bookapp/BookApp.java
+++ 2_Modules/src/main/bookapp/BookApp.java
@@ -13,19 +13,19 @@ public class BookApp {
     BookstoreService store = new BookstoreServiceImpl(booksService);
 
     // Create some books
-    int id1 = booksService.createBook("Java 9 Modularity in Action", 45.0d);
+    int id1 = booksService.createBook("Java 9 Modularity", 45.0d);
     int id2 = booksService.createBook("Modular Cloud Apps with OSGi", 40.0d);
     printf("Created books with id [%d, %d]", id1, id2);
 
     // Retrieve them again
     Book book1 = booksService.getBook(id1);
     Book book2 = booksService.getBook(id2);
-    printf("Retrieved books:\n  %d: %s\n  %d: %s",
-      id1, book1.getTitle(), id2, book2.getTitle());
+    printf("Retrieved books:\n  %d: %s [%.2f]\n  %d: %s [%.2f]",
+      id1, book1.getTitle(), book1.getPrice(), id2, book2.getTitle(), book2.getPrice());
 
     // Use the other service to calculate a total
     double total = store.calculatePrice(id1, id2);
-    printf("Total price: %1$.2f", total);
+    printf("Total price (with tax): %.2f", total);
   }
 
   private static void printf(String msg, Object... args) {
--- 3_MonolithicSpring/src/bookapp/main/Main.java
+++ 3_MonolithicSpring/src/bookapp/main/Main.java
@@ -8,39 +8,38 @@ import bookstore.api.service.BookstoreService;
 
 public class Main {
 
-	public void start() {
-		System.out.println("Starting...");
+  public void start() {
+    System.out.println("Starting...");
 
-		ApplicationContext context =
-    		new ClassPathXmlApplicationContext(new String[] {"classpath:/main.xml"});
+    ApplicationContext context =
+        new ClassPathXmlApplicationContext(new String[] {"classpath:/main.xml"});
 
 
-		BooksService booksService = context.getBean(BooksService.class);
-		BookstoreService store = context.getBean(BookstoreService.class);
+    BooksService booksService = context.getBean(BooksService.class);
+    BookstoreService store = context.getBean(BookstoreService.class);
 
-    	// Create some books
-	    int id1 = booksService.createBook("Java 9 Modularity in Action", 45.0d);
-	    int id2 = booksService.createBook("Modular Cloud Apps with OSGi", 40.0d);
-	    printf("Created books with id [%d, %d]", id1, id2);
+      // Create some books
+      int id1 = booksService.createBook("Java 9 Modularity", 45.0d);
+      int id2 = booksService.createBook("Modular Cloud Apps with OSGi", 40.0d);
+      printf("Created books with id [%d, %d]", id1, id2);
 
-	    // Retrieve them again
-	    Book book1 = booksService.getBook(id1);
-	    Book book2 = booksService.getBook(id2);
-	    printf("Retrieved books:\n  %d: %s\n  %d: %s",
-	      id1, book1.getTitle(), id2, book2.getTitle());
+      // Retrieve them again
+      Book book1 = booksService.getBook(id1);
+      Book book2 = booksService.getBook(id2);
+      printf("Retrieved books:\n  %d: %s [%.2f]\n  %d: %s [%.2f]",
+        id1, book1.getTitle(), book1.getPrice(), id2, book2.getTitle(), book2.getPrice());
 
-	    // Use the other service to calculate a total
-	    double total = store.calculatePrice(id1, id2);
-	    printf("Total price: %1$.2f", total);
+      // Use the other service to calculate a total
+      double total = store.calculatePrice(id1, id2);
+      printf("Total price (with tax): %.2f", total);
 
-    	
-	}
+  }
 
-	public static void main(String[] args) {
-		new Main().start();
-	}
+  public static void main(String[] args) {
+    new Main().start();
+  }
 
-	private void printf(String msg, Object... args) {
-    	System.out.println(String.format(msg + "\n", args));
-  	}
+  private void printf(String msg, Object... args) {
+      System.out.println(String.format(msg + "\n", args));
+    }
 }

37657c3f4a8e6db5e9ba277bee021f21c86e4c48
Paul Bakker <paul.bakker@luminis.eu>
2016-10-03 10:01:40 +0200

    Automatic modules for demo 3

--- 3_MonolithicSpring/src/bookapp/module-info.java
+++ 3_MonolithicSpring/src/bookapp/module-info.java
@@ -1,2 +1,11 @@
-module bookapp {	
+module bookapp {
+	requires spring.context;
+	requires spring.tx;
+	
+	requires javax.inject;
+	
+	requires hibernate.core;
+	requires hibernate.jpa;
+	
+	requires java.naming;	
 }

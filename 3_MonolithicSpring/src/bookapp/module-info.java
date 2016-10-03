a746268cf9ca41cc07d73301586ac85dfe762736
Paul Bakker <paul.bakker@luminis.eu>
2016-10-03 09:53:27 +0200

    Cleaning demo 3

--- 3_MonolithicSpring/src/bookapp/module-info.java
+++ 3_MonolithicSpring/src/bookapp/module-info.java
@@ -1,11 +1,2 @@
-weak module bookapp {
-	requires spring.context;
-	requires spring.tx;
-	
-	requires javax.inject;
-	
-	requires hibernate.core;
-	requires hibernate.jpa;
-	
-	requires java.naming;	
+module bookapp {	
 }
